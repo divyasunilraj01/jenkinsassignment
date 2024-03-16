54b2ffa58e77a65635a52adee0a23ce118df2d77
Divya <glishasunil@gmail.com>
2024-03-16 22:06:24 +0000

    conflict resolved

--- src/main/java/com/conn/DBConnect.java
+++ src/main/java/com/conn/DBConnect.java
@@ -12,15 +12,10 @@ public class DBConnect
 	{
 		try {
 			
-<<<<<<< HEAD
-			Class.forName("com.mysql.jdbc.Driver");
-		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
-			conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
-=======
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		//	conn = DriverManager.getConnection("jdbc:mysql:jassignment.db");
 			conn = DriverManager.getConnection("jdbc:mysql:C:/Users/Divya/eclipse-workspace/servers/E-Commerce/mydatabase.db");
->>>>>>> feature1
+
 			
 		}catch (Exception e) {
 			e.printStackTrace();
